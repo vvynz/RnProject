32926a33679d5bdf6cc62edd8d3a2cc52aed89c5
Vivian Zhang <vvynz12@gmail.com>
2023-06-28 19:07:32 -0400

    Styling update

--- App.js
+++ App.js
@@ -19,14 +19,29 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, world!</Text>
-    </View>
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView style={backgroundStyle}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgrouncColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Text>Hello, world!</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
