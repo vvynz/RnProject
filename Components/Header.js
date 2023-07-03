72a9d2296dffbbf958ded34bf328b4adf353a545
Vivian Zhang <vvynz12@gmail.com>
2023-07-03 15:13:17 -0400

    Minor styling updates

--- Components/Header.js
+++ Components/Header.js
@@ -3,8 +3,21 @@ import {View, Text, StyleSheet} from 'react-native';
 
 export default function Header() {
   return (
-    <View>
-      <Text>Shopping List</Text>
+    <View style={styles.header}>
+      <Text style={styles.text}>Shopping List</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    height: 50,
+    padding: 15,
+    backgroundColor: '#272727',
+  },
+  text: {
+    color: '#ddbbff',
+    textAlign: 'center',
+    fontSize: 18,
+  },
+});
