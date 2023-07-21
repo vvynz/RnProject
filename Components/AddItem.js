8ca225f8077f8123715ae583d450affe931e96c9
Vivian Zhang <vvynz12@gmail.com>
2023-07-20 22:45:11 -0400

    Setup and style the AddItem form

--- Components/AddItem.js
+++ Components/AddItem.js
@@ -6,7 +6,29 @@ import {
   StyleSheet,
   View,
 } from 'react-native';
+import {create} from 'react-test-renderer';
 
 export default function AddItem() {
-  return <Text>Add item</Text>;
+  return (
+    <TouchableOpacity>
+      <View style={styles.addItemView}>
+        <TextInput style={styles.addItemInput} />
+        <Text>+</Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
+
+const styles = StyleSheet.create({
+  addItemView: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  addItemInput: {
+    height: 40,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: '#a5a4a4',
+    padding: 10,
+  },
+});
